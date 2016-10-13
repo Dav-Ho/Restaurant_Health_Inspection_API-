971b3fb5d63bc58d506fff346d26455a981172be
david <david@davids-MacBook-Pro-2.local>
2016-10-13 15:07:43 -0400

    Install AngularJS and loop through Public API data

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RestaurantsController < ApplicationController
   def index
-    @response = Unirest.get('https://data.cityofnewyork.us/resource/xx67-kt59.json').body
+    # @response = Unirest.get('https://data.cityofnewyork.us/resource/xx67-kt59.json').body
   end
 end
